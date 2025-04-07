b513bf6c5e03aacaa8d0c4488f6430adf18571c8
lAlejandrocd <lalejandrocd1@gmail.com>
2025-04-06 21:16:34 -0500

    he agregado un span

--- index.php
+++ index.php
@@ -17,5 +17,11 @@ $conn = new Conn();
 ?>
 <h1>he creado un ejemeplo de push con pull request</h1>
 
+<p>
+    <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus quas suscipit minus nesciunt perferendis! Quasi pariatur vero nisi perferendis assumenda corporis inventore quae veniam debitis quaerat obcaecati, non nulla. Fuga!</span>
+    <span>Molestiae, vero! Architecto fuga nam necessitatibus vitae nostrum, aliquam nemo earum laborum, reiciendis quos ex dolor odit commodi placeat nobis, at eos libero consectetur sit. Ipsa dignissimos maiores molestiae laborum.</span>
+    <span>Delectus eius quam quisquam, ab perferendis deleniti corrupti nihil? Inventore odio deleniti, voluptatum tenetur nihil architecto eligendi ipsa in maiores? Ipsam suscipit est quidem, nobis quam labore vel modi praesentium.</span>
+</p>
+
 </body>
 </html>
